cf724de7dfe17c172df89c4620951087b17ab282
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-03-27 10:52:42 +0200

    20240327

--- complex/tlu.c
+++ complex/tlu.c
@@ -9,8 +9,8 @@
 int
 main()
 {
-	complex_t A[9], decA[9], iA[9], AiA[9];
-	complex_t Pm[9], L[9], U[9], PA[9], rPA[9];
+	complex_t A[9], Adec[9], Ainv[9], AAinv[9];
+	complex_t Pm[9], L[9], U[9], PA[9], LU[9];
 	complex_t x[3], b[3], y[3];
 	complex_t t0;
 	long P[4];
@@ -31,24 +31,24 @@ main()
 	mkC(b[1], 0.0, 1.0);
 	mkC(b[2], 0.5, 0.5);
 	
-	copyAB(3, decA, A);
+	copyAB(3, Adec, A);
 	
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
 			printf("A[%d][%d] = ", i, j);
 			printc("", MIJ(3, A, i, j), "\n");
 		}
-	r = LUPDecompose(3, decA, P, 0.000000001);
+	r = LUPDecompose(3, Adec, P, 0.000000001);
 	assert(r);
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
-			printf("\tdecA[%d][%d] = ", i, j);
-			printc("", MIJ(3, decA, i, j), "\n");
+			printf("\tAdec[%d][%d] = ", i, j);
+			printc("", MIJ(3, Adec, i, j), "\n");
 		}
 	for (i = 0; i <= 3; i++)
 		printf("\tP[%d] = %ld\n", i, P[i]);
 	
-	LUPExtract(3, Pm, L, U, decA, P);
+	LUPExtract(3, Pm, L, U, Adec, P);
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
 			printf("Pm[%d][%d] = ", i, j);
@@ -59,34 +59,34 @@ main()
 			printc("", MIJ(3, U, i, j), "\n");
 		}
 	mulCAB(3, PA, Pm, A);
-	mulCAB(3, rPA, L, U);
+	mulCAB(3, LU, L, U);
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
-			printf("PA[%d][%d] = ", i, j);
+			printf("P*A[%d][%d] = ", i, j);
 			printc("", MIJ(3, PA, i, j), " | ");
-			printf("rPA[%d][%d] = ", i, j);
-			printc("", MIJ(3, rPA, i, j), "\n");
+			printf("L*U[%d][%d] = ", i, j);
+			printc("", MIJ(3, LU, i, j), "\n");
 		}
 	
-	t0 = LUPDeterminant(3, decA, P);
-	printf("det(A) = ");
+	t0 = LUPDeterminant(3, Adec, P);
+	printf("detA = ");
 	printc("", t0, "\n");
 	
-	LUPInvert(3, iA, decA, P);
+	LUPInvert(3, Ainv, Adec, P);
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
-			printf("iA[%d][%d] = ", i, j);
-			printc("", MIJ(3, iA, i, j), "\n");
+			printf("Ainv[%d][%d] = ", i, j);
+			printc("", MIJ(3, Ainv, i, j), "\n");
 		}
 	
-	mulCAB(3, AiA, A, iA);
+	mulCAB(3, AAinv, A, Ainv);
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) {
-			printf("AiA[%d][%d] = ", i, j);
-			printc("", MIJ(3, AiA, i, j), "\n");
+			printf("A*Ainv[%d][%d] = ", i, j);
+			printc("", MIJ(3, AAinv, i, j), "\n");
 		}
 	
-	LUPSolve(3, decA, P, x, b);
+	LUPSolve(3, Adec, P, x, b);
 	for (i = 0; i < 3; i++) {
 		printf("x[%d] =", i);
 		printc("", x[i], "\n");
