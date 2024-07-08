f3162913602c7fae7fb224d9a87b40cad99a1b70
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-07-08 13:45:57 +0300

    2024070800

--- XENIX/complex/complex.h
+++ XENIX/complex/complex.h
@@ -54,8 +54,7 @@ typedef struct _complex_t {
 } while (0)
 
 #define swapZ2(Z, U) do { \
-	register double __ReU__; \
-	__ReU__ = Re(U); \
+	double __ReU__ = Re(U); \
 	Re(Z) = Im(U); \
 	Im(Z) = __ReU__; \
 } while (0)
@@ -92,33 +91,33 @@ typedef struct _complex_t {
 #define subZ2(Z, U) subZ3(Z, Z, U)
 
 #define mulZ3(Z, U, V) do { \
-	register double __ReU__ = Re(U); \
-	register double __ReV__ = Re(V); \
+	double __ReU__ = Re(U); \
+	double __ReV__ = Re(V); \
 	Re(Z) = __ReU__ * __ReV__ - Im(U) * Im(V); \
 	Im(Z) = Im(U) * __ReV__ + __ReU__ * Im(V); \
 } while (0)
 #define mulZ2(Z, U) mulZ3(Z, Z, U)
 
 #define innerZ3(Z, U, V) do { \
-	register double __ReU__ = Re(U); \
-	register double __ReV__ = Re(V); \
-	register double __nImV__ = -Im(V); \
+	double __ReU__ = Re(U); \
+	double __ReV__ = Re(V); \
+	double __nImV__ = -Im(V); \
 	Re(Z) = __ReU__ * __ReV__ - Im(U) * __nImV__; \
 	Im(Z) = Im(U) * __ReV__ + __ReU__ * __nImV__; \
 } while (0)
 #define innerZ2(Z, U) innerZ3(Z, Z, U)
 
 #define maddZ(Z, U, V) do { \
-	register double __ReU__ = Re(U); \
-	register double __ReV__ = Re(V); \
+	double __ReU__ = Re(U); \
+	double __ReV__ = Re(V); \
 	Re(Z) += __ReU__ * __ReV__ - Im(U) * Im(V); \
 	Im(Z) += Im(U) * __ReV__ + __ReU__ * Im(V); \
 } while (0)
 
 #define divZ3(Z, U, V) do { \
-	register double __ReU__ = Re(U); \
-	register double __ReV__ = Re(V); \
-	register double __magsqV__ = magsqZ(V); \
+	double __ReU__ = Re(U); \
+	double __ReV__ = Re(V); \
+	double __magsqV__ = magsqZ(V); \
 	Re(Z) = (__ReU__ * __ReV__ + Im(U) * Im(V)) / __magsqV__; \
 	Im(Z) = (Im(U) * __ReV__ - __ReU__ * Im(V)) / __magsqV__; \
 } while (0)
@@ -130,21 +129,21 @@ typedef struct _complex_t {
 } while (0)
 
 #define expiZ2(Z, U) do { \
-	register double __ReU__ = Re(U); \
+	double __ReU__ = Re(U); \
 	Re(Z) = exp(-Im(U)) * cos(__ReU__); \
 	Im(Z) = exp(-Im(U)) * sin(__ReU__); \
 } while (0)
 #define expiZ(Z) expiZ2(Z, Z)
 
 #define expZ2(Z, U) do { \
-	register double __ReU__ = Re(U); \
+	double __ReU__ = Re(U); \
 	Re(Z) = exp(__ReU__) * cos(Im(U)); \
 	Im(Z) = exp(__ReU__) * sin(Im(U)); \
 } while (0)
 #define expZ(Z) expZ2(Z, Z)
 
 #define logZ2(Z, U, BR) do { \
-	register double __argU__ = argZ(U, BR); \
+	double __argU__ = argZ(U, BR); \
 	Re(Z) = 0.5 * log(magsqZ(U)); \
 	Im(Z) = __argU__; \
 } while (0)
@@ -155,7 +154,7 @@ typedef struct _complex_t {
 #define LogZ(Z) logZ(Z, 0)
 #else
 #define LogZ2(Z, U) do { \
-	register double __ArgU__ = ArgZ(U); \
+	double __ArgU__ = ArgZ(U); \
 	Re(Z) = 0.5 * log(magsqZ(U)); \
 	Im(Z) = __ArgU__; \
 } while (0)
@@ -163,34 +162,34 @@ typedef struct _complex_t {
 #endif
 
 #define normZ2(Z, U) do { \
-	register double __ArgU__ = ArgZ(U); \
+	double __ArgU__ = ArgZ(U); \
 	expiphiZ(Z, __ArgU__); \
 } while (0)
 #define normZ(Z) normZ2(Z, Z)
 
 #define SinZ2(Z, U) do { \
-	register double __expImU__ = exp(Im(U)); \
+	double __expImU__ = exp(Im(U)); \
 	Im(Z) = -0.5 * (1.0 / __expImU__ - __expImU__) * cos(Re(U)); \
 	Re(Z) = 0.5 * (1.0 / __expImU__ + __expImU__) * sin(Re(U)); \
 } while (0)
 #define SinZ(Z) SinZ2(Z, Z)
 
 #define CosZ2(Z, U) do { \
-	register double __expImU__ = exp(Im(U)); \
+	double __expImU__ = exp(Im(U)); \
 	Im(Z) = 0.5 * (1.0 / __expImU__ - __expImU__) * sin(Re(U)); \
 	Re(Z) = 0.5 * (1.0 / __expImU__ + __expImU__) * cos(Re(U)); \
 } while (0)
 #define CosZ(Z) CosZ2(Z, Z)
 
 #define SinhZ2(Z, U) do { \
-	register double __expReU__ = exp(Re(U)); \
+	double __expReU__ = exp(Re(U)); \
 	Re(Z) = 0.5 * (__expReU__ - 1.0 / __expReU__) * cos(Im(U)); \
 	Im(Z) = 0.5 * (__expReU__ + 1.0 / __expReU__) * sin(Im(U)); \
 } while (0)
 #define SinhZ(Z) SinhZ2(Z, Z)
 
 #define CoshZ2(Z, U) do { \
-	register double __expReU__ = exp(Re(U)); \
+	double __expReU__ = exp(Re(U)); \
 	Re(Z) = 0.5 * (__expReU__ + 1.0 / __expReU__) * cos(Im(U)); \
 	Im(Z) = 0.5 * (__expReU__ - 1.0 / __expReU__) * sin(Im(U)); \
 } while (0)
