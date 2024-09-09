46c4b921029feac3a277da0abc491a5c00bf4e30
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-09-09 17:50:35 +0300

    2024090900 Watcom C

--- XENIX/complex/cconfig.h
+++ XENIX/complex/cconfig.h
@@ -14,4 +14,22 @@
 #undef __DEBUG_CI_D__
 #endif
 
+#ifdef __WATCOMC__
+#ifndef M_PI
+#define M_PI		3.14159265358979323846	/* pi */
+#endif
+#ifndef M_PI_2
+#define M_PI_2		1.57079632679489661923	/* pi/2 */
+#endif
+#ifndef M_PI_4
+#define M_PI_4		0.78539816339744830962	/* pi/4 */
+#endif
+#ifndef M_LN2
+#define M_LN2		0.69314718055994530942	/* log_e 2 */
+#endif
+#ifndef M_LN10
+#define M_LN10		2.30258509299404568402	/* log_e 10 */
+#define
+#endif
+
 #endif
